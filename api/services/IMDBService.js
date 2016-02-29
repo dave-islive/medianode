ddde76758fbe52be90f8caa7047bd9646edf8665
deef <dave@islivebv.nl>
2016-02-29 02:22:34 +0100

    stopped

--- api/services/IMDBService.js
+++ api/services/IMDBService.js
@@ -1,10 +1,36 @@
 var Promise = require('bluebird'),
     omdb    = require('omdb');
 
+// TODO - Build better api interface http://www.omdbapi.com/
+
 var IMDBService = {
   search: function (search) {
     return new Promise(function (resolve, reject) {
-      omdb(search, function (error, result) {
+      omdb.search(search, function (error, result) {
+        if (error) {
+          return reject(error);
+        }
+
+        resolve(result);
+      });
+    });
+  },
+
+  getInfo: function (id) {
+    return new Promise(function (resolve, reject) {
+      omdb.get(id, function (error, result) {
+        if (error) {
+          return reject(error);
+        }
+
+        resolve(result);
+      });
+    });
+  },
+
+  getPoster: function (id) {
+    return new Promise(function (resolve, reject) {
+      omdb.poster(id, function (error, result) {
         if (error) {
           return reject(error);
         }
